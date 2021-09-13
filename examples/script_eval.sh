15d7504381d2f41ff01439461fdbbfd402c95faf
IvanoLauriola <ivanolauriola@gmail.com>
2021-09-13 15:02:30 +0200

    fix modelname script eval

--- examples/script_eval.sh
+++ examples/script_eval.sh
@@ -1,5 +1,5 @@
 python run_eval.py \
-       --model ../../models/contextual/asnq/electra-base-discriminator.local-ord.asnq.lr:1e+05-p:2-b:512-e:15-s:256-w:2-r:1.pt \
+       --model my-finetuned-model.pt \
        --context local-ord \
        --cuda 0 \
        --test ../data/squad2.0-dev.csv \
