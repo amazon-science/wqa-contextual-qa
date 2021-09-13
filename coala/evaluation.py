bug fix: MAP computation when dealing with unanswerable questions; new features: 1) Trainer accepts eval sets, 2) new metrics, v0.1.1

coala/evaluation.py
@@ -10,7 +10,8 @@ import numpy as np
 from sklearn.metrics import roc_auc_score
 
 
-def report(labels, scores, questions):
+
+def report(labels, scores, questions, return_metadata=True):
     '''computes various statistics and metrics'''
     results = {}
     if len(np.unique(labels)) == 2:
@@ -19,49 +20,59 @@ def report(labels, scores, questions):
     for label, score, q in zip(labels, scores, questions):
         dict_questions[q]['labels'].append(label)
         dict_questions[q]['scores'].append(score)
-    results['p@1'] = np.mean([precision_at_k(v['scores'], v['labels'], 1, 1) for v in dict_questions.values()])
-    results['map'] = np.mean([average_precision(v['scores'], v['labels'], 1) for v in dict_questions.values()])
-    results['questions'] = len(dict_questions)
-    results['examples']  = len(labels)
+    results['P@1'] = np.mean([precision_at_k(v['labels'], v['scores'], 1) for v in dict_questions.values()])
+    results['MAP'] = np.mean([average_precision(v['labels'], v['scores']) for v in dict_questions.values()])
+    results['MRR'] = np.mean([reciprocal_rank(v['labels'], v['scores']) for v in dict_questions.values()])
+    results['HIT@3'] = np.mean([hit_at_k(v['labels'], v['scores'], 3) for v in dict_questions.values()])
+    results['HIT@5'] = np.mean([hit_at_k(v['labels'], v['scores'], 5) for v in dict_questions.values()])
+    results['AUPC'] = area_under_precision_curve(labels, scores, questions)
+    if return_metadata:
+        results['questions'] = len(dict_questions)
+        results['examples']  = len(labels)
     return results
 
 
-def reciprocal_rank(scores, labels, pos_label=None):
-    '''reciprocal rank given a query'''
-    pos_label = pos_label or max(labels)
-    scores, labels = zip(*sorted(zip(scores, labels),reverse=True))
-    n = len(scores)
 
-    return 1./(labels.index(1)+1)
-    #return sum([1./i for i in range(n) if labels[i] == pos_label])
+### ranking metrics for single question (question + all candidates)
 
-
-def average_precision(scores, labels, pos_label=None):
-    '''average precision given a query'''
-    pos_label = pos_label or max(labels)
+def precision_at_k(labels, scores, k, pos_label=1):
+    '''precision at k given a question'''
     scores, labels = zip(*sorted(zip(scores, labels),reverse=True))
     n =len(scores)
+    n_pos = labels[:k].count(pos_label)
+    return 0 if not n_pos else n_pos / k
 
+
+def hit_at_k(labels, scores, k, pos_label=1):
+    '''hit at k given a question'''
+    scores, labels = zip(*sorted(zip(scores, labels),reverse=True))
+    return int(pos_label in labels[:k])
+
+
+def average_precision(labels, scores, pos_label=1):
+    '''average precision given a question'''
+    scores, labels = zip(*sorted(zip(scores, labels),reverse=True))
+    n =len(scores)
     n_pos = labels.count(pos_label)
     if not n_pos:
-        return 1
-    ap = sum([precision_at_k(scores, labels, i+1, pos_label) if labels[i]==pos_label else 0 for i in range(n)]) / n_pos
-    #print (labels, n_pos, ap, [precision_at_k(scores, labels, i+1, pos_label) for i in range(6)])
+        return 0
+    ap = sum([precision_at_k(labels, scores, i+1, pos_label) if labels[i]==pos_label else 0 for i in range(n)]) / n_pos
     return ap
 
-def precision_at_k(scores, labels, k, pos_label=None):
-    '''precision at k'''
-    pos_label = pos_label or max(labels)
+
+def reciprocal_rank(labels, scores, pos_label=1):
+    '''reciprocal rank given a question'''
     scores, labels = zip(*sorted(zip(scores, labels),reverse=True))
-    n =len(scores)
-    
-    n_pos = labels[:k].count(pos_label)
-    if not n_pos:
-        return 0.
-    return sum([1. if l==pos_label else 0 for l in labels[:k]]) / k
+    return 1./(labels.index(pos_label)+1) if pos_label in labels else 0
 
 
-def precision_curve(labels, scores, questions):
+
+
+
+
+### global metrics (that involve all questions)
+
+def precision_curve(labels, scores, questions, pos_label=1):
     dict_questions = dict()
     for question, label, score in zip(questions, labels, scores):
         if question not in dict_questions or dict_questions[question]['score'] < score:
@@ -74,9 +85,14 @@ def precision_curve(labels, scores, questions):
     precision, answer_rate = [], []
     pos = 0
     for i, s, l in zip(range(1,len(m_labels)+1), m_scores, m_labels):
-        pos += l
+        pos += l==pos_label
         precision.append(pos*1./i)
         answer_rate.append(i/len(m_labels))
     thresholds = m_scores
 
     return precision, answer_rate, thresholds
+
+
+def area_under_precision_curve(labels, scores, questions, pos_label=1):
+    precision, answer_rate, thresholds = precision_curve(labels, scores, questions, pos_label=pos_label)
+    return sum(precision) / len(precision)

coala/models.py
@@ -38,6 +38,8 @@ class ModelForAS2(nn.Module):
                 
             if checkpoint.config.architectures[0].endswith('ForMaskedLM'):
                 logger.warning('The loaded model is converted from MLM to sequence classification. The classification head has to be fine-tuned')
+                print (checkpoint)
+                print (checkpoint.config)
                 as2model = AutoModelForSequenceClassification.from_pretrained(checkpoint.config._name_or_path)
                 as2encoder = getattr(as2model, checkpoint.config.model_type)
                 as2encoder.embeddings = checkpoint.embeddings
@@ -67,7 +69,7 @@ class ModelForAS2(nn.Module):
         torch.save(self, out_path)
 
         
-    def to_huggingface():
+    def to_huggingface(self):
         '''convert the model to huggingface transformers'''
         return self.transformer
 

coala/trainer.py
@@ -27,12 +27,13 @@ class AS2Trainer():
             epochs    = 3,
             patience  = 2,
             loss_fct  = nn.CrossEntropyLoss(),
-            val_metric= 'p@1', #p@1, roc_auc, val_loss
+            val_metric= 'P@1', #p@1, roc_auc, val_loss
             debug     = False,
             save_path = None,
             device    = 'cpu',
     ):
-        assert val_metric in ['p@1','roc_auc','loss', 'val_loss'], 'Evaluation Metric not recognized: %s' % val_metric
+        assert val_metric in ['loss','val_loss','roc_auc','P@1','MAP','MRR','HIT@5','AUPC'], \
+            'Evaluation Metric not recognized: %s' % val_metric
         self.logger = utils.get_logger(debug)
 
         self.model     = model
@@ -47,7 +48,7 @@ class AS2Trainer():
         self.device    = device
 
         
-    def fit(self, dataloader, dataloader_va):
+    def fit(self, dataloader, dataloader_va, dataloaders_eval=[]):
 
         
         epochs_without_improvement = 0
@@ -63,6 +64,8 @@ class AS2Trainer():
         if dataloader_va:
             self.logger.info('Validation data found (%d examples)' % len(dataloader_va.dataset))
             self.logger.info('The training will end when the %s computed on validation data worsens for %d consecutive epochs' % (self.val_metric, self.patience))
+        if dataloaders_eval:
+            self.logger.info('Evaluation data found (%d examples)' % sum(len(dd.dataset) for dd in dataloaders_eval))
         if self.debug:
             self.logger.debug('THE TRAINER IS RUNNING IN DEBUG MODE!')
 
@@ -104,24 +107,26 @@ class AS2Trainer():
             loss_tr /= len(dataloader)
             
             time_eval = time.time()
-            
-            y_scores, loss_va = self.predict(dataloader_va, return_loss=True)
-            y_true = dataloader_va.dataset.labels[:len(y_scores)]
-            questions_va = dataloader_va.dataset.questions[:len(y_scores)]
-            results = evaluation.report(y_true, y_scores, questions=questions_va)
-            results['tr_loss'] = loss_tr
-            results['val_loss'] = loss_va
+
+            results = []
+            for dloader in [dataloader_va] + dataloaders_eval:
+                y_scores, loss_eval = self.predict(dloader, return_loss=True)
+                y_true = dloader.dataset.labels[:len(y_scores)]
+                questions_eval = dloader.dataset.questions[:len(y_scores)]
+                ds_results = evaluation.report(y_true, y_scores, questions=questions_eval, return_metadata=False)
+                ds_results['loss'] = loss_eval
+                results.append(ds_results)
             time_eval = time.time() - time_eval
                 
-            if self.val_metric in ['p@1', 'roc_auc']:
-                op = lambda x,y: x < y
-            else:
+            if 'loss' in self.val_metric:
                 op = lambda x,y: x > y
+            else:
+                op = lambda x,y: x < y
 
             epochs_without_improvement += 1
             is_saved = ''
-            if not best_metrics or op(best_metrics[self.val_metric], results[self.val_metric]):
-                best_metrics = results
+            if not best_metrics or op(best_metrics[self.val_metric], results[0][self.val_metric]):
+                best_metrics = results[0]
                 epochs_without_improvement = 0
                 if self.save_path:
                     #self.model.save(self.save_path)
@@ -132,10 +137,15 @@ class AS2Trainer():
                     is_saved = ' (saved)'
 
             time_all = time.time() - time_all
-            self.logger.info('Epoch %d - %s: %.4f%s' % (epoch, self.val_metric, results[self.val_metric], is_saved ))
-            report_str = ', '.join('%s:%.4f' % (k,v) for k,v in results.items())
-            self.logger.info('    Evaluation report: %s' % report_str)
-            self.logger.info('    The epoch has been executed in %.1fs, of which %.1fs training and %.1fs evaluation' % (time_all, time_epoch, time_eval))
+            self.logger.info('Epoch %d - %s: %.4f%s' % (epoch, self.val_metric, results[0][self.val_metric], is_saved ))
+            self.logger.info('    Execution time: %.1fs, of which %.1fs training and %.1fs evaluation' % (time_all, time_epoch, time_eval))
+            self.logger.info('    Evaluation report:')
+            self.logger.info('        training set - loss: %.4f, ' % loss_tr)
+            for report_id, report in enumerate(results):
+                report_str = ', '.join('%s:%d' % (k,v) if isinstance(v, int) else '%s:%.4f' % (k,v) for k,v in report.items())
+                eval_set_name = ' ' * 8 + ('validation set - ' if not report_id else ('eval set [#%d] - ' % report_id))
+                self.logger.info(eval_set_name + report_str)
+
             if epochs_without_improvement >= self.patience :
                 self.logger.info ('Early stopping')
                 sys.stdout.flush()

examples/run_eval.py
@@ -12,6 +12,7 @@ import torch
 import argparse, os
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model',         default='roberta-base',   help='model path')
+parser.add_argument('-tok', '--tokenizer',   default=None,              help='tokenizer name or path (optional)')
 parser.add_argument('-x', '--context',       default='base',           help='context type',
                     choices=['base','local', 'local-ord'])
 parser.add_argument('-c', '--cuda',          default=-2,   type=int,   help='assigned gpu id, -1 means all, -2 means CPU')
@@ -37,8 +38,9 @@ model = model_class(args.model)
 model_name = model.transformer.config._name_or_path #e.g. roberta-base, bert-base-uncased
 
 from transformers import AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-logger.info('Using %s tokenizer' % model_name) 
+tokenizer_name_or_path = args.tokenizer or model_name
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path)
+logger.info('Using %s tokenizer' % tokenizer_name_or_path)
 
 device = torch.device('cpu' if args.cuda==-2 else 'cuda')
 if args.cuda >=0:
@@ -69,7 +71,7 @@ questions = dataset_te.questions
 from coala.evaluation import report
 results = report(labels, scores, questions)
 logger.info('Evaluation report:')
-logger.info('  ' + ', '.join('%s:%d' % (k,v) if type(v)==int else '%s:%.4f' % (k,v) for k,v in results.items()))
+logger.info('    ' + ', '.join('%s:%d' % (k,v) if type(v)==int else '%s:%.4f' % (k,v) for k,v in results.items()))
 
 from coala.evaluation import precision_curve
 pre, cov, thr = precision_curve(labels, scores, questions)

examples/run_training.py
@@ -11,24 +11,27 @@ import torch
 
 import argparse, os
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--model',         default='bert-base',      help='model name or path')
-parser.add_argument('-x', '--context',       default='base',           help='context type',
+parser.add_argument('-m', '--model',         default='bert-base',       help='model name or path')
+parser.add_argument('-tok', '--tokenizer',   default=None,              help='tokenizer name or path (optional)')
+parser.add_argument('-x', '--context',       default='base',            help='context type',
                     choices=['base','local', 'local-ord'])
-parser.add_argument('-c', '--cuda',          default=-2,   type=int,   help='assigned gpu id, -1 means all, -2 means CPU')
-parser.add_argument('-t', '--training',      required=True,            help='training data file')
-parser.add_argument('-v', '--validation',    required=False,           help='validation data file')
-parser.add_argument('-e', '--max_epochs',    default=5,    type=int,   help='max number of training epochs')
-parser.add_argument('-w', '--workers',       default=0,    type=int,   help='num threads for data generation')
-parser.add_argument('-r', '--run',           default=0,    type=int,   help='current run')
-parser.add_argument('-b', '--batch_size',    default=32,   type=int,   help='global batch size')
-parser.add_argument('-l', '--learningrate',  default=1e-5, type=float, help='learning rate')
-parser.add_argument('-s', '--max_seq_length',default=128,  type=int,   help='max sequence length (256 for contextual models is sufficient)')
-parser.add_argument('-o', '--output_file',   default=None,             help='output file name')
-parser.add_argument('-p', '--patience',      default=5,    type=int,   help='earlystopping epochs')
-parser.add_argument('--warmup_peak',         default=.2,   type=float, help='warmup peak in epochs')
-parser.add_argument('--debug',               action='store_true',      help='if true, shows debug information')
-parser.add_argument('--val_metric',          default='p@1',            help='validation metric',
-                    choices=['loss','val_loss','roc_auc','p@1'])
+parser.add_argument('-c', '--cuda',          default=-2,   type=int,    help='assigned gpu id, -1 means all, -2 means CPU')
+parser.add_argument('-t', '--training',      required=True,  nargs='+',
+                    help='training data file(s). In case of multiple files, they are joined during training')
+parser.add_argument('-v', '--validation',    required=False, nargs='+',
+                    help='validation data file(s). In case of multiple files, metrics are computed for each of them but only the first is used as validation set')
+parser.add_argument('-e', '--max_epochs',    default=5,    type=int,    help='max number of training epochs')
+parser.add_argument('-w', '--workers',       default=0,    type=int,    help='num threads for data generation')
+parser.add_argument('-r', '--run',           default=0,    type=int,    help='current run')
+parser.add_argument('-b', '--batch_size',    default=32,   type=int,    help='global batch size')
+parser.add_argument('-l', '--learningrate',  default=1e-5, type=float,  help='learning rate')
+parser.add_argument('-s', '--max_seq_length',default=128,  type=int,    help='max sequence length (256 for contextual models is sufficient)')
+parser.add_argument('-o', '--output_file',   default=None,              help='output file name')
+parser.add_argument('-p', '--patience',      default=5,    type=int,    help='earlystopping epochs')
+parser.add_argument('--warmup_peak',         default=.2,   type=float,  help='warmup peak in epochs')
+parser.add_argument('--debug',               action='store_true',       help='if true, shows debug information')
+parser.add_argument('--val_metric',          default='P@1',             help='validation metric',
+                    choices=['loss','val_loss','roc_auc','P@1','MAP','MRR','HIT@5','AUPC'])
 args = parser.parse_args()
 print(args)
 
@@ -45,8 +48,9 @@ model = model_class(args.model)
 model_name = model.transformer.config._name_or_path #e.g. roberta-base, bert-base-uncased
 
 from transformers import AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-logger.info('Using %s tokenizer' % model_name) 
+tokenizer_name_or_path = args.tokenizer or model_name
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path)
+logger.info('Using %s tokenizer' % tokenizer_name_or_path)
 
 device = torch.device('cpu' if args.cuda==-2 else 'cuda')
 if args.cuda >=0:
@@ -61,17 +65,22 @@ model.to(device)
 
 
 from coala.loader import read_dataset
-data_tr = read_dataset(args.training)
-data_va = read_dataset(args.validation)
+data_tr = []
+for path in args.training:
+    data_tr.extend(read_dataset(path))
+data_va = read_dataset(args.validation[0])
+data_eval = [read_dataset(path) for path in args.validation[1:]]
 
 dataset_tr = dataset_class(data_tr, tokenizer, max_seq_len=args.max_seq_length)
 dataset_va = dataset_class(data_va, tokenizer, max_seq_len=args.max_seq_length)
+datasets_eval = [dataset_class(_data, tokenizer, max_seq_len=args.max_seq_length) for _data in data_eval]
 
 from torch.utils.data import DataLoader
 from coala.utils import get_batch_padding_as2
 my_padding_fn = get_batch_padding_as2(tokenizer.pad_token_id)
 dataloader_tr = DataLoader(dataset_tr, batch_size=args.batch_size, shuffle=True,  num_workers=args.workers, collate_fn=my_padding_fn)
-dataloader_va =	DataLoader(dataset_va, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, collate_fn=my_padding_fn)
+dataloader_va =	DataLoader(dataset_va, batch_size=args.batch_size*2, shuffle=False, num_workers=args.workers, collate_fn=my_padding_fn)
+dataloaders_eval = [DataLoader(ds_eval, batch_size=args.batch_size*2, shuffle=False, num_workers=args.workers, collate_fn=my_padding_fn) for ds_eval in datasets_eval]
 
 
 from transformers import get_linear_schedule_with_warmup
@@ -96,7 +105,7 @@ trainer = Trainer(
     debug     = args.debug,
     save_path = args.output_file,
     device    = device,
-).fit(dataloader_tr, dataloader_va)
-
+).fit(dataloader_tr, dataloader_va, dataloaders_eval)
+print (len(dataloaders_eval))
 
     